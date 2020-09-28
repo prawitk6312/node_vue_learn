e1bff6af63e4c8c5ad47d8018951eb310827391e
prawitk6312 <prawitk6312@gmail.com>
2020-09-28 18:55:37 +0700

    setinterval lean

---
+++ setinterval.js
@@ -0,0 +1,9 @@
+//การใช้งาน setTimeout, setInterval, clearTimeout, clearInterval
+
+let x = setInterval(function() {
+    console.log('function setinterval')
+}, 1000)
+
+setTimeout(function(){
+    clearInterval(x)
+}, 2000)
